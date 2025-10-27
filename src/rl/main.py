75d58eeef9dc9e342e2298397460a01fcd8e74d8
Konrad Brüggemann <konrad.brueggemann@uni-potsdam.de>
2025-10-27 14:53:40 +0100

    add data loader

--- src/rl/main.py
+++ src/rl/main.py
@@ -1,7 +1,9 @@
 from agent import get_trainer
+from get_data import prepare_rl_data
 from wandb import login
 
 if __name__ == "__main__":
     login()
+    prepare_rl_data()
     trainer = get_trainer()
     trainer.train()
